f3cbebbcf3bf6f29ddf8dd95cbd7e66f2d26b905
Riday <rdmondal100@gmail.com>
2025-03-03 13:51:45 +0600

    completed the loop challenges

---
+++ Javascripts/JsChallenges/03-Loops/loop-01.js
@@ -0,0 +1,12 @@
+// You just need to implement the distributeGifts function
+function distributeGifts(totalGifts, friends) {
+    // write your code here
+    let distributeGifts = 0
+    for (let i = 0; i < totalGifts; i++) {
+        if (distributeGifts === friends) {
+            return distributeGifts
+        }
+        distributeGifts++
+    }
+    return distributeGifts
+}
