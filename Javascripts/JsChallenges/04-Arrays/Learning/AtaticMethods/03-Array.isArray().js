78a974f6cd81f318a77138b3aa6bcec4f2ebcbec
Riday Mondal <123723569+rdmondal100@users.noreply.github.com>
2025-03-10 13:53:59 +0600

    Projects (#13)
    * Completed kanban board initially
    
    * the home page of the repo
    
    * edit
    
    * edit the folder name
    
    * Removed submodule reference

---
+++ Javascripts/JsChallenges/04-Arrays/Learning/AtaticMethods/03-Array.isArray().js
@@ -0,0 +1,39 @@
+console.log(Array.isArray([1, 3, 5]));
+// Expected output: true
+
+console.log(Array.isArray("[]"));
+// Expected output: false
+
+console.log(Array.isArray(new Array(5)));
+// Expected output: true
+
+console.log(Array.isArray(new Int16Array([15, 33])));
+// Expected output: false
+
+
+
+
+
+
+// all following calls return true
+Array.isArray([]);
+Array.isArray([1]);
+Array.isArray(new Array());
+Array.isArray(new Array("a", "b", "c", "d"));
+Array.isArray(new Array(3));
+// Little known fact: Array.prototype itself is an array:
+Array.isArray(Array.prototype);
+
+// all following calls return false
+Array.isArray();
+Array.isArray({});
+Array.isArray(null);
+Array.isArray(undefined);
+Array.isArray(17);
+Array.isArray("Array");
+Array.isArray(true);
+Array.isArray(false);
+Array.isArray(new Uint8Array(32));
+// This is not an array, because it was not created using the
+// array literal syntax or the Array constructor
+Array.isArray({ __proto__: Array.prototype });
