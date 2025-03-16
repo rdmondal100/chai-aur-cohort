8e19ffa70d50373f83eaf56f3bd93eff65163a90
Riday <rdmondal100@gmail.com>
2025-03-16 10:22:54 +0600

    challenge two done

--- DOM/DOMChallenges/01-Dom-Challenge/stackblitz-starters-i4bf8zdv/challenge-2/app.js
+++ DOM/DOMChallenges/01-Dom-Challenge/stackblitz-starters-i4bf8zdv/challenge-2/app.js
@@ -1,3 +1,36 @@
 /**
  * Write your challenge solution here
  */
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const mainHeading = document.getElementById("mainHeading")
+
+    const btnContainer = document.querySelector(".color-buttons")
+    console.log(btnContainer)
+
+
+    if (btnContainer) {
+        btnContainer.addEventListener('click', (e) => {
+            if (e.target.matches("button")) {
+                let color = e.target.innerText
+                if (color === "Reset") {
+                    color = "Black"
+                }
+                if (color) {
+
+                    changeMainHeadingColor(color)
+                }
+            }
+        })
+    }
+
+    const changeMainHeadingColor = (color) => {
+        if (color) {
+            mainHeading.style.color = color
+        }
+    }
+
+
+})
