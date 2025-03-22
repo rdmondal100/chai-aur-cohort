7f4d1cb1fd5903cdf6a807d02b02ea6c8c73ce36
Riday Mondal <123723569+rdmondal100@users.noreply.github.com>
2025-03-22 16:07:13 +0600

    Dom (#18)
    * Completed kanban board initially
    
    * the home page of the repo
    
    * edit
    
    * edit the folder name
    
    * Removed submodule reference
    
    * challenge one done
    
    * challenge two done
    
    * challenge three done
    
    * challenge four done
    
    * challenge five completed

--- DOM/DOMChallenges/01-Dom-Challenge/stackblitz-starters-i4bf8zdv/challenge-3/app.js
+++ DOM/DOMChallenges/01-Dom-Challenge/stackblitz-starters-i4bf8zdv/challenge-3/app.js
@@ -1,3 +1,33 @@
 /**
  * Write your challenge solution here
  */
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const formContainer = document.querySelector(".form-container")
+    const inputsArray = Array.from(formContainer.querySelectorAll('input'))
+    inputsArray.push(formContainer.querySelector("textarea"))
+    console.log(inputsArray)
+
+    // const nameInput = document.getElementById("nameInput")
+    // const jobInput = document.getElementById("jobInput")
+    // const ageInput = document.getElementById("ageInput")
+    // const bioInput = document.getElementById("bioInput")
+
+    if(inputsArray.length){
+        inputsArray.map((inputItem)=>{
+            const index = String(inputItem.id).search('Input')
+            console.log(index)
+            const DisPlayNameId = inputItem.id.slice(0,Number(index)) +"Display"
+            console.log(DisPlayNameId)
+            inputItem.addEventListener('input',(e)=>{
+                const displayElement = document.getElementById(DisPlayNameId)
+                console.log(displayElement)
+                let currentInputValue = String(e.target.value).trim()
+                displayElement.innerText = currentInputValue || "Not provided"
+            })
+        })
+    }
+
+})
